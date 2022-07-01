e18b9d07409af7f1a6597b1d129566fcaae64b82
Andrii Sotskyi <andrewsotskyi@gmail.com>
2022-07-01 17:10:28 -0500

    fix: customize scrollbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -42,6 +42,17 @@ const useStyles = makeStyles((theme) => ({
     padding: '0px 72px 0px',
     boxSizing: 'border-box',
     overflowY: 'scroll',
+    '&::-webkit-scrollbar': {
+      width: '5px',
+    },
+    '&::-webkit-scrollbar-track': {
+      boxShadow: 'inset 0 0 6px rgba(0,0,0,0.00)',
+      webkitBoxShadow: 'inset 0 0 6px rgba(0,0,0,0.00)',
+    },
+    '&::-webkit-scrollbar-thumb': {
+      backgroundColor: 'rgba(0,0,0,.1)',
+      outline: '1px solid slategrey',
+    },
     height: '545px',
     display: 'flex',
     justifyContent: 'space-between',
